1b9490e4691bcadca87a88e45f31e80f8a15d85e
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-13 16:07:38 +0100

    VUE create and use component NavBar in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,24 @@
 <script>
+import Navbar from './Navbar.vue';
+
 export default {
 
   data() {
     return {
       menuItems: ['Home', 'Courses', 'Instructors', 'Event', 'Pages', 'Elements'],
     }
+  },
+
+  components: {
+    Navbar
   }
 }
 </script>
 
 <template>
   <header>
-    <!-- Inizio NavBar -->
-    <div class="container">
-      <nav>
-        <img class="logo" src="/image/logo-light.png" alt="">
 
-        <div class="menu">
-          <ul class="links">
-            <li v-for="item in menuItems"> {{ item }}</li>
-          </ul>
-
-          <ul class="icons">
-            <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
-            <li><font-awesome-icon icon="fa-solid fa-bag-shopping" /></li>
-            <li><font-awesome-icon icon="fa-solid fa-bars" /></li>
-          </ul>
-        </div>
-
-
-      </nav>
-    </div>
-    <!-- Fine NavBar -->
+    <Navbar :items="menuItems" />
 
     <!-- Inizio Hero -->
     <div class="hero">
@@ -51,35 +38,5 @@ header {
   background-image: url('/image/course-9-f-img.jpg');
   background-repeat: no-repeat;
   background-size: cover;
-}
-
-nav {
-  padding: 20px 0;
-  font-family: "Open Sans", sans-serif;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  .logo {
-    width: 160px;
-  }
-
-  .menu {
-    display: flex;
-    gap: 30px;
-    font-size: 15px;
-    font-weight: 400px;
-
-    .links {
-      display: flex;
-      gap: 40px;
-      text-transform: uppercase;
-    }
-
-    .icons {
-      display: flex;
-      gap: 20px;
-    }
-  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+
+  props: {
+
+    items: {
+      type: Array,
+      required: true
+    }
+
+  }
+}
+</script>
+
+<template>
+  <!-- Inizio NavBar -->
+  <div class="container">
+    <nav>
+      <img class="logo" src="/image/logo-light.png" alt="">
+
+      <div class="menu">
+        <ul class="links">
+          <li v-for="item in items"> {{ item }}</li>
+        </ul>
+
+        <ul class="icons">
+          <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
+          <li><font-awesome-icon icon="fa-solid fa-bag-shopping" /></li>
+          <li><font-awesome-icon icon="fa-solid fa-bars" /></li>
+        </ul>
+      </div>
+
+    </nav>
+  </div>
+  <!-- Fine NavBar -->
+</template>
+
+<style lang="scss" scoped>
+nav {
+  padding: 20px 0;
+  font-family: "Open Sans", sans-serif;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    width: 160px;
+  }
+
+  .menu {
+    display: flex;
+    gap: 30px;
+    font-size: 15px;
+    font-weight: 400px;
+
+    .links {
+      display: flex;
+      gap: 40px;
+      text-transform: uppercase;
+    }
+
+    .icons {
+      display: flex;
+      gap: 20px;
+    }
+  }
+}
+</style>
