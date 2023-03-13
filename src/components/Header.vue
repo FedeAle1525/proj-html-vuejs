b1e541d67e31d846ccd42a5f45b413db12b4c42d
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-13 17:16:51 +0100

    VUE create and use component Hero in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <script>
 import Navbar from './Navbar.vue';
+import Hero from './Hero.vue';
 
 export default {
 
@@ -10,7 +11,8 @@ export default {
   },
 
   components: {
-    Navbar
+    Navbar,
+    Hero
   }
 }
 </script>
@@ -20,65 +22,17 @@ export default {
 
     <Navbar :items="menuItems" />
 
-    <!-- Inizio Hero -->
-    <div class="container">
-      <div class="hero">
-        <span class="title">Contemporary Ideas</span>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatum minima necessitatibus expedita ratione harum magni dolore quia blanditiis nulla culpa reiciendis
-          soluta recusandae exercitationem iusto,
-          assumenda veniam molestiae maxime quos.
-        </p>
-        <div class="button">Register Now</div>
-      </div>
-    </div>
-
-    <!-- Fine Hero -->
+    <Hero />
 
   </header>
 </template>
 
 <style lang="scss" scoped>
-@use '../style/partials/variables' as *;
-
-//1 - Importo Font Google
-@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap');
-//2 - Creo Variabile con Font importato
-$font-merriweather: 'Merriweather', serif;
-
 header {
   color: white;
   background-image: url('/image/course-9-f-img.jpg');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50% 55%;
-}
-
-.hero {
-  display: flex;
-  gap: 20px;
-  flex-direction: column;
-  align-items: center;
-  font-family: "Open Sans", sans-serif;
-  padding: 150px 30px;
-
-  .title {
-    font-family: $font-merriweather;
-    font-size: 63px;
-  }
-
-  p {
-    font-size: 16px;
-    line-height: 30px;
-    text-align: center;
-  }
-
-  .button {
-    text-transform: uppercase;
-    font-size: 14px;
-    font-weight: 700;
-    padding: 20px 30px;
-    background-color: $light-blue;
-  }
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <!-- Inizio Hero -->
+  <div class="container">
+    <div class="hero">
+      <span class="title">Contemporary Ideas</span>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatum minima necessitatibus expedita ratione harum magni dolore quia blanditiis nulla culpa reiciendis
+        soluta recusandae exercitationem iusto,
+        assumenda veniam molestiae maxime quos.
+      </p>
+      <div class="button">Register Now</div>
+    </div>
+  </div>
+
+  <!-- Fine Hero -->
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+//1 - Importo Font Google
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap');
+//2 - Creo Variabile con Font importato
+$font-merriweather: 'Merriweather', serif;
+
+.hero {
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Open Sans", sans-serif;
+  padding: 150px 30px;
+
+  .title {
+    font-family: $font-merriweather;
+    font-size: 63px;
+  }
+
+  p {
+    font-size: 16px;
+    line-height: 30px;
+    text-align: center;
+  }
+
+  .button {
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 700;
+    padding: 20px 30px;
+    background-color: $light-blue;
+  }
+}
+</style>
