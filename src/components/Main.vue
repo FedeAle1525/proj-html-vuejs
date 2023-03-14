2ec1180aad2b52013cf64c974825eed86a77b8ed
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-14 11:01:47 +0100

    VUE create and use component Tutoring in Main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,20 @@
 <script>
 import Types from './Types.vue';
+import Tutoring from './Tutoring.vue';
 
 export default {
 
   components: {
     Types,
+    Tutoring
   }
 }
 </script>
 
 <template>
   <Types />
+
+  <Tutoring />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/Tutoring.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+
+}
+</script>
+
+
+<template>
+  <section>
+    <div class="container">
+      <span>The Trusted Name <br>
+        for In-Home Tutoring.
+      </span>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure repudiandae fugit rem sint veniam quam doloremque
+        dolorum beatae expedita alias ducimus soluta quos, explicabo mollitia quidem quisquam vel pariatur qui.
+      </p>
+
+      <a href="#">Learn More</a>
+
+      <img src="/image/h5-img-1.jpg">
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+//1 - Importo Font Google
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap');
+//2 - Creo Variabile con Font importato
+$font-merriweather: 'Merriweather', serif;
+
+.container {
+  padding: 50px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  font-family: 'Open Sans', sans-serif;
+
+  span {
+    font-family: $font-merriweather;
+    font-size: 37px;
+  }
+
+  p {
+    font-size: 17px;
+    font-weight: 400;
+    line-height: 26px;
+  }
+
+  a {
+    font-size: 14px;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: $light-blue;
+  }
+
+  img {
+    max-width: 90%;
+  }
+}
+</style>
